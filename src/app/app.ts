87ec8859e8155d55143c6026e4ece78f5b889aae
gnganapath <gnganapathi@gmail.com>
2026-07-30 00:50:11 +0530

    Add GitHub Actions deployment

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('angular21tour');
+  protected readonly title = signal('angular21tour -signal');
 }
